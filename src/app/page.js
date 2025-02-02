24f03d7bbd68e94ced249a93a187183fc3a35e50
Sanjan Athyady <sanjanathyady8@gmail.com>
2025-02-02 17:15:53 +0530

    feat: simple landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
-import Image from "next/image";
-
-export default function Home({ params, searchParams }) {
-  const newParam = searchParams.new;
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+export default function Home() {
   return (
     <>
-      <h1>
-        hello {params.id}
-        {newParam}
-      </h1>
+      <div className="container flex flex-col text-black text-3xl m-10  items-center gap-10 justify-center min-h-screen py-2">
+        <h1>Welcome to the FAQ page!</h1>
+        <p>
+          This is the FAQ page. Here, you can find answers to frequently asked
+          questions.
+        </p>
+        <div className="flex gap-4">
+          <Link href="/admin">
+            <Button className="">Admin Panel</Button>
+          </Link>
+          <Link href="/faq">
+            <Button>FAQ</Button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
